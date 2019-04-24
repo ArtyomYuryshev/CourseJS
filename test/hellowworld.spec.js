f26823b7fd459a01a44b971c00b47b467cc3e929
ArtyomYuryshev <a2.yuryshev@gmail.com>
2019-04-24 21:02:55 +0300

    DZ1

--- test/hellowworld.spec.js
+++ test/hellowworld.spec.js
@@ -1,5 +1,6 @@
 const { describe, it } = require('mocha');
 const logger = require('../utils/log.util')
+//const { describe, it } = require('chai');
 
 describe('Hellow World TestSuite', () => {
     it('should write "Hellow World"',() => {
